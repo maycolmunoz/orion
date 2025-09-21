1b22f829614ec1863cacb0ba485a8d62869925c4
maycolmunoz <estivenyeizon@gmail.com>
2025-09-21 09:35:25 -0500

    feat: 🎸 add metrics in dashboard

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages;
 
+use Composer\InstalledVersions;
 use MoonShine\Contracts\UI\ComponentContract;
 use MoonShine\Laravel\Pages\Page;
+use MoonShine\UI\Components\Layout\Grid;
+use MoonShine\UI\Components\Metrics\Wrapped\ValueMetric;
 
 class Dashboard extends Page
 {
@@ -24,11 +27,43 @@ class Dashboard extends Page
         return $this->title ?: 'Dashboard';
     }
 
+    private function version(string $pkg): string
+    {
+        return InstalledVersions::getPrettyVersion($pkg);
+    }
+
     /**
      * @return list<ComponentContract>
      */
     protected function components(): iterable
     {
-        return [];
+        return [
+            Grid::make([
+                ValueMetric::make('PHP')
+                    ->value(fn () => PHP_VERSION)
+                    ->columnSpan(4)
+                    ->icon('s.cube'),
+
+                ValueMetric::make('Laravel')
+                    ->value(fn () => $this->version('laravel/framework'))
+                    ->columnSpan(4)
+                    ->icon('s.cube'),
+
+                ValueMetric::make('Modular')
+                    ->value(fn () => $this->version('internachi/modular'))
+                    ->columnSpan(4)
+                    ->icon('s.cube'),
+
+                ValueMetric::make('MoonShine')
+                    ->value(fn () => $this->version('moonshine/moonshine'))
+                    ->columnSpan(4)
+                    ->icon('s.cube'),
+
+                ValueMetric::make('Roles & Permissions')
+                    ->value(fn () => $this->version('sweet1s/moonshine-roles-permissions'))
+                    ->columnSpan(4)
+                    ->icon('s.cube'),
+            ]),
+        ];
     }
 }
